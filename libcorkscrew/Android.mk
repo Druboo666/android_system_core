2b839a48da051422880abb8391a9839f7e7b2c91
Faradis <DrubO@ovi.com>
2013-10-12 11:52:56 +0600

    GCC-4.8+ Build Fix

--- libcorkscrew/Android.mk
+++ libcorkscrew/Android.mk
@@ -51,7 +51,8 @@ endif
 
 LOCAL_SHARED_LIBRARIES += libdl libcutils liblog libgccdemangle
 
-LOCAL_CFLAGS += -std=gnu99 -Werror -Wno-unused-parameter -fno-inline-small-functions -Wno-error=unused-parameter
+LOCAL_CFLAGS += -std=gnu99 -Werror -fno-inline-small-functions -Wno-error=unused-parameter
+
 LOCAL_MODULE := libcorkscrew
 LOCAL_MODULE_TAGS := optional
 
